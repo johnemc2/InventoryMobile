17d7d2120f6b7ce149fa2c69392635be17ddbf72
johnemc2 <jonathangrice@yahoo.com>
2018-09-22 21:44:07 -0500

    Initial commit

---
+++ app/src/main/java/com/jonathanryangrice/inventorymobile/MainApplication.java
@@ -0,0 +1,13 @@
+package com.jonathanryangrice.inventorymobile;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+    public static final String LOG_TAG = "AppAuthSample";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
