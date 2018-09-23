f920dbb916c30e8daf10ea8c0d3ab71ce10d2261
johnemc2 <jonathangrice@yahoo.com>
2018-09-22 22:06:36 -0500

    Update

--- app/src/main/java/com/jonathanryangrice/inventorymobile/MainActivity.java
+++ app/src/main/java/com/jonathanryangrice/inventorymobile/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
         final AuthState authState = new AuthState(response, error);
 
         if (response != null) {
-            Log.i(LOG_TAG, String.format("Handled Authorization Response %s ", authState.toJsonString()));
+            Log.i(LOG_TAG, String.format("Handled Authorization Response %s ", authState.toJsonString())); 
             AuthorizationService service = new AuthorizationService(this);
             service.performTokenRequest(response.createTokenExchangeRequest(), new AuthorizationService.TokenResponseCallback() {
                 @Override
